cdecd9d99936902deb1057967b5576e0465acb1d
Rhianna Stuart <rhiannastuart@hotmail.co.uk>
2020-05-29 00:30:44 +0100

    FINALLY!

--- jscript.js
+++ jscript.js
@@ -2,21 +2,28 @@
 fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => addToWallList(data))
 
 function addToWallList(list){
-    list.forEach(function (item){
-        let list2 = document.createdElement('li')
-        let updatedList = documentTextNode(item)
+    list.forEach(function (element){
+        let list2 = document.createElement('li')
+        let updatedList = document.createTextNode(element)
         list2.appendChild(updatedList)
         let currentList = document.getElementById('list')
         document.body.insertBefore(list2, currentList)
-        console.log(item)
-      
+
     })
 
 } 
-
 
 function addtoList(){
     let newItem = document.getElementById('test')
     list.push(addToWallList())
     console.log(newItem)
 }
+const ul = document.querySelector(lists)
+const url = 'https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall'
+
+
+
+
+
+
+
